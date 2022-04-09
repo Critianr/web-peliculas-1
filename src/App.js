a94f10f7547538d0092278228ee5955973102edc
unknown <calrod350@gmail.com>
2022-04-09 17:05:18 -0500

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {#, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { ListaVista } from "./components/ListaVista";
 // import { Watched } from "./components/Watched";
