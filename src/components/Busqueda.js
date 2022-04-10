8f330fe2b232df0b2714e82706532226afd42407
unknown <calrod350@gmail.com>
2022-04-09 23:32:00 -0500

    react-create-app on Heroku4

--- src/components/Busqueda.js
+++ src/components/Busqueda.js
@@ -11,27 +11,27 @@ export const Busqueda = () => {
 
     setQuery(e.target.value);
 
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c978912fb46ea0658038ca141f934d90&language=en-US&page=1&include_adult=false&query=${e.target.value}`,{
-    header:{'Accept': 'application/json'}
-    }
-   )
-    .then(res => {
-      const data = res.data;
-      setResults(data.results)
-    })
+  //   axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c978912fb46ea0658038ca141f934d90&language=en-US&page=1&include_adult=false&query=${e.target.value}`,{
+  //   header:{'Accept': 'application/json'}
+  //   }
+  //  )
+  //   .then(res => {
+  //     const data = res.data;
+  //     setResults(data.results)
+  //   })
   
-    // fetch(
-    //   `https://api.themoviedb.org/3/search/movie?api_key=c978912fb46ea0658038ca141f934d90&language=en-US&page=1&include_adult=false&query=${e.target.value}`
-    //   )
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     if (!data.errors) {
-    //       setResults(data.results);
-    //     } else {
-    //       setResults([]);
-    //     }
-    //     console.log(data)
-    //   });
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=c978912fb46ea0658038ca141f934d90&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          setResults(data.results);
+        } else {
+          setResults([]);
+        }
+        console.log(data)
+      });
   };
 
   return (
