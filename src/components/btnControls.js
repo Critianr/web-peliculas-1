2cc13ac82569747d08644b36bda91824451fa0b9
unknown <calrod350@gmail.com>
2022-04-10 23:16:52 -0500

    react-create-app on Heroku3

--- src/components/btnControls.js
+++ src/components/btnControls.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { GlobContext } from "../context/statesGlobales";
 
-export const btnControls = ({ type, movie }) => {
+export const MovieControls = ({ type, movie }) => {
   const {
     eliminarPelicula,
     addListPelicula,
